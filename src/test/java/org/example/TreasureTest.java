80f489fc4125e440dd16819e50edbce500aefa2d
hamza.hassine <hamza.hassine@supcom.tn>
2017-12-12 22:04:09 +0100

    run tests in emulation mode

--- src/test/java/org/example/TreasureTest.java
+++ src/test/java/org/example/TreasureTest.java
@@ -1,57 +1,50 @@
 package org.example;
 
-import org.infinispan.creson.Factory;
 import org.infinispan.creson.test.Emulation;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.Random;
 
-public class TreasureTest {
-    Graph gr;
-    Hero superMan;
-    Hero batMan ; 
-    Thread b ; 
-    Thread s;
-    @BeforeMethod
-    public void setUp() throws Exception {
-        
-        Factory.get("localhost:11222");
-        gr = new Graph(1000);
-        Random random = new Random(System.currentTimeMillis());
-        for(int i = 0 ; i < gr.getGraphSize() ; i++) {
-            //if (i>0) gr.addUndirectedPath(i,random.nextInt(i));
-            gr.addUndirectedPath(i,gr.randomRoom(0, 10));
-            gr.addUndirectedPath(i,gr.randomRoom(0, 10));
-            gr.addUndirectedPath(i,gr.randomRoom(0, 10));
-            //System.out.println(gr.toString());
-        }
+public class TreasureTest extends Emulation {
+  Graph gr;
+  Hero superMan;
+  Hero batMan;
+  Thread b;
+  Thread s;
 
-        superMan = new Hero("SuperMan", gr);
-        batMan = new Hero("BatMan", gr);
+  @BeforeMethod
+  public void setUp() throws Exception {
+
+    gr = new Graph(1000);
+    for (int i = 0; i < gr.getGraphSize(); i++) {
+      gr.addUndirectedPath(i, gr.randomRoom(0, 10));
+      gr.addUndirectedPath(i, gr.randomRoom(0, 10));
+      gr.addUndirectedPath(i, gr.randomRoom(0, 10));
     }
 
-    @Test
-    public void should_rank_heros() {
-        s = new Thread(superMan);
-        b = new Thread(batMan);
-        s.start();
-        b.start();
-        try {
-          s.join();
-          b.join();
-        } catch (InterruptedException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-        }
-        
-        System.out.println(superMan.getScore());
-        System.out.println(batMan.getScore());
-       // assert superMan.getScore() == gr.nodes.size();
+    superMan = new Hero("SuperMan", gr);
+    batMan = new Hero("BatMan", gr);
+  }
+
+  @Test
+  public void should_rank_heros() {
+    s = new Thread(superMan);
+    b = new Thread(batMan);
+    s.start();
+    b.start();
+    try {
+      s.join();
+      b.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
 
-    /*@Override
-    protected int numberOfCaches(){
-        return 3;
-    }*/
+    System.out.println(superMan.getScore());
+    System.out.println(batMan.getScore());
+  }
+
+  @Override
+  protected int numberOfCaches() {
+    return 3;
+  }
 }
